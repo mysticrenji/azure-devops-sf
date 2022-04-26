6d01e9860630d52ce6403da9b4d28ec1385f7ea2
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2022-04-26 16:26:31 +0000

    Update variables

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-module "azure-devops" {
-  source              = "./modules/az-devops"
-  project             = var.project
-  project_description = var.project_description
-}
+# module "azure-devops" {
+#   source              = "./modules/az-devops"
+#   project             = var.project
+#   project_description = var.project_description
+# }
 
 module "service-fabric-managed" {
   source         = "./modules/service-fabric-managed"
